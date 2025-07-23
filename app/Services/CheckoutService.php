761e25c749c720908db783a4d1dd5d2fc1bbcff3
Kenyarm <Barathannadar@gmail.com>
2025-07-24 02:21:02 +0530

    Add missing Cart model import in CheckoutService

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Payment;
 use App\Models\Product;
+use App\Models\Cart;
 use Illuminate\Validation\ValidationException;
 use App\Events\OrderPlaced;
 use App\Events\PaymentSucceeded;
